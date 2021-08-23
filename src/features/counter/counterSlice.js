0b697fff2dce23dcea4805b8cd9194b5e0b64fb8
haider <haiderlek@gmail.com>
2021-08-23 09:10:18 +0300

    commit tree 9234f0f1dd1f5fdc63fd0fbd2c5d6971a3ee917a

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,17 +1,17 @@
-const initialState = {
-  items: [],
-};
+// const initialState = {
+//   items: [],
+// };
 
-function counterReducer(state = initialState, action) {
-  switch (action.type) {
-    case "count/increment":
-      return {
-        ...state,
-        items: state.items.concat(state.items.length + 1),
-      };
-    default:
-      return state;
-  }
-}
+// function counterReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case "count/increment":
+//       return {
+//         ...state,
+//         items: state.items.concat(state.items.length + 1),
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
-export default counterReducer;
+// export default counterReducer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import counterReducer from "./features/counter/counterSlice.js";
+import counterReducer from "./features/ShoppingList/shoppingListSlice";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
